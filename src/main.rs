3608ee71b046c4de2904382fbbd59dad9d703680
Arnaud Meunier <arnaudmeunier02@gmail.com>
2024-09-17 15:43:36 +0200

    [ADD] User input

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,33 @@
 mod quine_cluskey;
 
+use std::io;
+
 use quine_cluskey::QuineCluskey;
 
 fn main() {
-    let data: Vec<u8> = vec![2,3,4,10,12,13];
-    let test: QuineCluskey = QuineCluskey::new(data, Some(vec![11,14,15]));
-    println!("{}", test);
+    let data = get_user_input();
+    let quine: QuineCluskey = QuineCluskey::new(data.0, Some(data.1));
+    println!("{}", quine);
+}
+
+fn get_user_input () -> (Vec<u8>, Vec<u8>) {
+    let mut input_minterms: String = String::new();
+    let mut input_dont_care: String = String::new();
+    
+    println!("Enter minterms between 0 and 255 separated by a coma (',')");
+    io::stdin().read_line(&mut input_minterms).expect("Failed to read minterms input");
+
+    println!("Enter the don't care terms between 0 and 255 separated by a coma (',')\nNote that if a term is both a minterm and a don't care, the term will be considered a minterm");
+    io::stdin().read_line(&mut input_dont_care).expect("Failed to read don't care input");
+
+    return (convert_input_into_minterms(input_minterms), convert_input_into_minterms(input_dont_care));
+}
+
+fn convert_input_into_minterms(input: String) -> Vec<u8> {
+    
+    input
+        .trim()
+        .split(',')
+        .filter_map(|s| s.trim().parse::<u8>().ok())
+        .collect()
 }
